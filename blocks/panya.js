293051f93f0bab7b40d050c991dd8ae7155708e0
Muuo Wambua <muuowambua@yahoo.com>
2013-12-26 11:33:09 +0300

    started out on the custom blocks

---
+++ blocks/panya.js
@@ -0,0 +1,43 @@
+/**
+ * Visual Blocks Editor
+ *
+ * Copyright 2012 Google Inc.
+ * http://blockly.googlecode.com/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Variable blocks for Blockly.
+ * @author fraser@google.com (Neil Fraser)
+ */
+'use strict';
+
+goog.provide('Blockly.Blocks.variables');
+
+goog.require('Blockly.Blocks');
+
+
+Blockly.Blocks['panya_forward'] = {
+  init: function() {
+    this.setHelpUrl('http://www.example.com/');
+    this.setColour(160);
+    this.appendValueInput("Displacement")
+        .setCheck("Number")
+        .appendTitle("Move")
+        .appendTitle(new Blockly.FieldDropdown([["forward", "forward"], ["fackward", "reverse"]]), "Direction");
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setTooltip('');
+  }
+};
